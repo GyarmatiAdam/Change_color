953547119fa3b8c585d6d721d8ffe18b05ab1364
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-04-17 13:27:14 +0200

    Change color
    background color changing by clicking a button
    simple html, css, javascript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script
+//choose or select an element->make variable
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBackground = document.querySelector('body');
+
+//create an array, which holds the background colors
+const colors = ['red', 'green', 'pink', 'blue'];
+
+//add an event listener, which changes the color by clicing the button
+//eventlistener has event an callback function
+colorBtn.addEventListener('click', changeColor);
+
+//create callback function
+function changeColor (){
+    let random = Math.floor(Math.random() * colors.length)
+    bodyBackground.style.backgroundColor = colors[random];
+}
